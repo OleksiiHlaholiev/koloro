1541f6043859e0f44bad4dfa20995ee6abcb023c
oleksii.hlaholiev <oleksii.hlaholiev@gmail.com>
2017-06-13 15:02:40 +0300

    update

--- ante/php/send.php
+++ ante/php/send.php
@@ -6,7 +6,7 @@
         ){ 
     //Проверка отправилось ли наше поля name и не пустые ли они
         $to = 'oleksii.hlaholiev@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
-        $subject = 'Обратный звонок'; //Загаловок сообщения
+        $subject = 'Обратный звонок с сайта landos.ante.com.ua'; //Загаловок сообщения
         $message = '
                 <html>
                     <head>
@@ -19,7 +19,7 @@
                     </body>
                 </html>'; //Текст нащего сообщения можно использовать HTML теги
         $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
-        $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
+        $headers .= "From: Отправитель <landos.ante@.com.ua>\r\n"; //Наименование и почта отправителя
         mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
     }
 ?>

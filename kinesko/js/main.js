913e8c2b8bb8b011ed108ee4c51fa638422364df
glalex <oleksii.hlaholiev@gmail.com>
2017-07-14 12:23:20 +0300

    update

--- kinesko/js/main.js
+++ kinesko/js/main.js
@@ -1,3 +1,78 @@
 /**
  * Created by glalex on 12.07.2017.
  */
+
+"use strict";
+
+//кнопка скролла вверх
+function backToTop() {
+    $(window).scroll(function () {
+        var bo = $(document).scrollTop();
+        var time_to_show = 600;	//$("#indeficator_toshow").offset().top;
+
+        if (bo >= time_to_show) {
+            $(".back-to-top").addClass("back-to-top-visible");
+        }
+        else {
+            $(".back-to-top").removeClass("back-to-top-visible");
+        }
+    });
+    $(".back-to-top").click(function (e) {
+        e.preventDefault();
+        $("html, body").animate({scrollTop: 0}, 1100);
+    });
+}
+
+backToTop();
+
+$(function () {
+
+    var searchBtnStatus = 0,
+        searchContainerInput = $(".search-container .input-container input")[0],
+        searchBtn = $(".search-container .search-btn")[0];
+
+    $(searchBtn).on("click", function (event) {
+        if (!searchBtnStatus) {
+            event.preventDefault();
+            // searchContainerInput.setAttribute("autofocus", "autofocus");
+            $(searchContainerInput).addClass("active").fadeIn(400);
+            searchBtnStatus = 1;
+        }
+        else {
+            if(searchContainerInput.value.length) {
+                event.preventDefault();
+                location.href = "/search_result.html";
+                // console.log(location.href);
+            }
+            // searchBtnStatus = 0;
+        }
+        event.stopPropagation();
+
+        // $(searchContainerInput).toggleClass("active").fadeToggle();
+    });
+
+    function searchFormClose (e) {
+        if ( e.keyCode === 27 ) {
+            // close search-field on ESC
+            if($(searchContainerInput).hasClass("active")) {
+                $(searchContainerInput).removeClass("active").fadeOut(400);
+                searchBtnStatus = 0;
+            }
+        }
+    }
+
+    document.addEventListener('keydown', searchFormClose);
+
+    $("body").click(function(event) {
+        if(!$(event.target).closest(searchContainerInput).length &&
+            $(searchContainerInput).hasClass("active")) {
+            if (searchBtnStatus) {
+                event.preventDefault();
+                $(searchContainerInput).removeClass("active").fadeOut(400);
+                // searchContainerInput.removeAttribute("autofocus");
+                searchBtnStatus = 0;
+            }
+        }
+    });
+
+});

46388bf6d503dd5f6f682e4dab98954ea2d54dbd
glalex <oleksii.hlaholiev@gmail.com>
2018-01-06 18:18:10 +0200

    update

--- test/js/company.js
+++ test/js/company.js
@@ -6,14 +6,14 @@ $(function () {
 
     // turn on team-member photo on hover
     memberInfoConts.on("mouseover", function () {
-        if (window.innerWidth <= 1000) {
+        if (window.innerWidth > 1000) {
             $(this.parentNode).find(".member-photo").addClass("active");
         }
     });
 
     // turn off team-member photo on mouseleave
     memberInfoConts.on("mouseleave", function () {
-        if (window.innerWidth <= 1000) {
+        if (window.innerWidth > 1000) {
             $(this.parentNode).find(".member-photo").removeClass("active");
         }
     });

0e346d81b22cf05158cab9da998781f5daec0614
glalex <oleksii.hlaholiev@gmail.com>
2018-01-08 01:37:15 +0200

    update

--- test/js/libs/jquery.custom-file-input.js
+++ test/js/libs/jquery.custom-file-input.js
@@ -34,7 +34,3 @@
 		.on( 'blur', function(){ $input.removeClass( 'has-focus' ); });
 	});
 })( jQuery, window, document );
-(function (e, t, n) {
-    var r = e.querySelectorAll("html")[0];
-    r.className = r.className.replace(/(^|\s)no-js(\s|$)/, "$1js$2")
-})(document, window, 0);

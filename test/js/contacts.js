399552e17ea2016b62af73bbba6ecfde1821cdeb
glalex <oleksii.hlaholiev@gmail.com>
2018-01-08 14:26:38 +0200

    update

--- test/js/contacts.js
+++ test/js/contacts.js
@@ -46,11 +46,17 @@ $(function () {
         contactForms = $(".contacts-forms .contact-form"),
         formSwitchItems = $(".form-switch li");
 
+    // initial states
+    $(formHeaders).addClass("animated fadeInDown");
+    $(contactForms).addClass("animated fadeInUp");
+
     $(formSwitchItems).on("click", function () {
         if (!$(this).hasClass("active")) {
             $(formSwitchItems).toggleClass("active");
             $(formHeaders).toggleClass("active");
+            $(formHeaders).addClass("animated fadeInDown");
             $(contactForms).toggleClass("active");
+            $(contactForms).addClass("animated fadeInUp");
         }
     })
 });

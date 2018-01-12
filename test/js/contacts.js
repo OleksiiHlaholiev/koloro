81cc56bc6ec04a05bb3230c6846aa3b5cc0cdf3e
glalex <oleksii.hlaholiev@gmail.com>
2018-01-12 14:49:44 +0200

    update

--- test/js/contacts.js
+++ test/js/contacts.js
@@ -1,19 +1,13 @@
 "use strict";
 
-// //= libs/jquery.custom-file-input.js
-// (function (e, t, n) {
-//     var r = e.querySelectorAll("html")[0];
-//     r.className = r.className.replace(/(^|\s)no-js(\s|$)/, "$1js$2")
-// })(document, window, 0);
-
 $(function () {
     var formHeaders = $(".form-headers-cont .form-header"),
         contactForms = $(".contacts-forms .contact-form"),
         formSwitchItems = $(".form-switch li");
 
     // initial states
-    $(formHeaders).addClass("animated fadeInDown");
-    $(contactForms).addClass("animated fadeInUp");
+    // $(formHeaders).addClass("animated fadeInDown");
+    // $(contactForms).addClass("animated fadeInUp");
 
     $(formSwitchItems).on("click", function () {
         if (!$(this).hasClass("active")) {

9077b1c98ca3bb9c855485ea77133f65f2595da9
glalex <oleksii.hlaholiev@gmail.com>
2018-01-13 17:22:32 +0200

    update

--- test/js/portfolio-material.js
+++ test/js/portfolio-material.js
@@ -5,15 +5,12 @@ $(function () {
     if (!isMobileViewFlag) {
         // ****************************************************************************
         // *************    ANIMATIONS FOR THIS PAGE    *************
-        
+
         $('.portfolio-material-cont .order-form-btn').addClass("invisible").viewportChecker({
             classToAdd: 'animated fadeInUp',
             classToRemove: 'visible',
             offset: '10%',
             repeat: false
         });
-
-
     }
-
 });

aecb5798aaf4b1c53f393ff90f86628423a8d9e8
glalex <oleksii.hlaholiev@gmail.com>
2018-02-06 19:50:30 +0200

    update

--- sticky_header/js/main.js
+++ sticky_header/js/main.js
@@ -4,13 +4,16 @@ window.addEventListener('load', function() {
 
     var stickyHeader = document.getElementById("sticky-header"),
         beforeHeaderSection = document.querySelector(".before-header-section"),
+        afterHeaderSection = document.querySelector(".after-header-section"),
         refPoint = stickyHeader.offsetTop;
 
     function scrollWindowHandler() {
         if (window.pageYOffset >= beforeHeaderSection.clientHeight) {
             stickyHeader.classList.add("sticky");
+            afterHeaderSection.style.paddingTop = stickyHeader.clientHeight + "px";
         } else {
             stickyHeader.classList.remove("sticky");
+            afterHeaderSection.style.paddingTop = "0";
         }
     }
     
